5ed8afe3dda344b032071524f40bfc48d037567c
Mohit Patel <mohit.patel@hotmail.com>
2021-08-25 19:45:10 -0400

    * remove test cases

--- test/factories.ts
+++
@@ -1,8 +0,0 @@
-import { Factory } from 'typeorm-factory';
-import { Todo } from './../src/todo/todo.entity';
-import * as Faker from 'faker';
-
-export const TodoFactory = new Factory(Todo)
-  .sequence('content', () => Faker.lorem.sentence())
-  .sequence('isVisible', () => Faker.random.boolean())
-  .sequence('isCompleted', () => Faker.random.boolean());
